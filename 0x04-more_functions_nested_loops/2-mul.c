e441ffb043fc5c3ee553d6597526831912ded658
Donald serome <donaldserome.ds@gmail.com>
2023-08-17 07:13:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
